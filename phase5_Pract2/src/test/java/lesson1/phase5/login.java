b86261bd2d361854c0dafeef929f806aea98328c
ARUL D <aruln311@gmail.com>
2022-03-13 00:01:59 +0530

    Add files via upload

---
+++ phase5_Pract2/src/test/java/lesson1/phase5/login.java
@@ -0,0 +1,38 @@
+package lesson1.phase5;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class login {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hp\\Desktop\\Java FSD\\phase5\\chromedriver.exe");
+     WebDriver driver = new ChromeDriver();
+//Register
+     driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.in/");
+		driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+		WebElement mail = driver.findElement(By.id("ap_email"));
+		mail.sendKeys("aruln311@gmail.com");
+		mail.submit();
+		WebElement password = driver.findElement(By.id("ap_password"));
+		password.sendKeys("Arul@0311");
+		driver.findElement(By.id("signInSubmit")).click();
+		
+		WebElement searchbox = driver.findElement(By.id("twotabsearchtextbox"));
+		searchbox.sendKeys("iPhone 13");
+		searchbox.submit();
+		driver.findElement(By.linkText("Apple iPhone 13 (512GB) - Starlight")).click();
+		driver.findElement(By.id("nav-cart-count-container")).click();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@id='submit.add-to-cart']")).click();
+		driver.findElement(By.id("nav-cart-count-container")).click();
+
+
+ }
+}
